6740ec23f3d09f7a03beb4b684ff1a55f2fe4dd9
Deepak <jangiddee999@gmail.com>
2025-10-09 01:59:53 +0530

    Update App.jsx

--- frontend/imaginara/src/App.jsx
+++ frontend/imaginara/src/App.jsx
@@ -7,7 +7,7 @@ import Login from "./pages/Auth/Login";
 import SignUp from "./pages/Auth/SignUp";
 import HomePage from "./pages/Artwork/ArtworkList";
 import ArtworkDetail from "./pages/Artwork/ArtworkDetail";
-import AddArtwork from "./pages/Artwork/Addartwork";
+import AddArtwork from "./pages/Artwork/AddArtwork";
 import ExplorePage from "./pages/Home/ExplorePage";
 import ProfilePage from "./pages/Profile/ProfilePage";
 import Settings from "./pages/Settings/Settings";
