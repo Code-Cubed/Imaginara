2861e767b4477d41afcf64eff3afbdf2cb56717c
psharma1280 <116288664+psharma1280@users.noreply.github.com>
2025-10-10 11:42:20 +0530

    Added Image Generator AI tool

--- frontend/imaginara/src/components/leftBar/leftBar.jsx
+++ frontend/imaginara/src/components/leftBar/leftBar.jsx
@@ -13,7 +13,8 @@ import {
     BarChart2,
     Palette,
     Sparkles,
-    Mail
+    Mail,
+    Wand2
 } from 'lucide-react'; 
 
 const LeftBar = ({ onLogout }) => {
@@ -87,6 +88,7 @@ const LeftBar = ({ onLogout }) => {
     {path:"/analytics", lucideIcon: BarChart2, alt:"Analytics"},
     {path:"/similarity-search", lucideIcon: Sparkles, alt:"Similarity Search"},
     { path: "/chatbot", lucideIcon: Bot, alt: "AI ChatBot" },
+    { path: "/image-generator", lucideIcon: Wand2, alt: "AI Image Generator" },
     {path:"/boards", lucideIcon: Users, alt:"Boards"},
     { path: "/settings", lucideIcon: Settings, alt: "Settings" },
   ];
