a1d4f9a66ac8bc517444b42a4ad63916437706b4
psharma1280 <116288664+psharma1280@users.noreply.github.com>
2025-10-11 02:00:41 +0530

    Modified Settings of Image Generator

---
+++ frontend/imaginara/src/pages/Constants/blockedKeywords.js
@@ -0,0 +1,11 @@
+// src/constants/blockedKeywords.js
+const blockedKeywords = [
+  "boob", "boobs", "nude", "vagina", "penis", "sex", "naked", 
+  "porn", "fuck", "breast", "dick", "cum", "orgasm", "ass", 
+  "pussy", "anal", "masturbate", "nsfw", "hentai", "hardcore", "pornographic", "sexual", "fetish", "kink",
+  "whore", "slut", "bitch", "dildo", "vibrator", "prostitute", "escort", "sex toy", "cumshot", "stripper", "lapdance", "bondage", "sado", "maso", "sadomasochism",
+  "blowjob", "bj", "cum", "sperm", "ejaculation", "penetration", "threesome", "anal", "oral", "vaginal", "handjob",
+
+];
+
+export default blockedKeywords;
